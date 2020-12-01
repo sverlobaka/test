925a4c10d9dd9a570001c35dbfbf008b40f35824
sverlobaka <73789971+sverlobaka@users.noreply.github.com>
2020-12-01 14:09:53 +0300

    Update Alphabet war.py

--- Alphabet war.py
+++ Alphabet war.py
@@ -3,55 +3,36 @@ def fight(text):
     >>> fight('')
     "Let's fight again!"
     >>> fight('abracadabra')
-    'Headless side wins!'
-    >>> fight('z')
-    'Right side wins!'
-    >>> fight('zdqmwpbs')
+    'army3 wins!'
+    >>> fight('wmu')
     "Let's fight again!"
-    >>> fight('zzzzs')
-    'Right side wins!'
-    >>> fight('wwwwwwz')
-    'Left side wins!'
-    >>> fight('wpbs')
-    'Left side wins!'
-    >>> fight('aeub')
-    'Headless side wins!'
-
     """
 
-    side = {
-        'w': {'left': 4, 'right': 0, 'headless': 0},
-        'p': {'left': 3, 'right': 0, 'headless': 0},
-        'b': {'left': 2, 'right': 0, 'headless': 0},
-        's': {'left': 1, 'right': 0, 'headless': 0},
-        'm': {'left': 0, 'right': 4, 'headless': 0},
-        'q': {'left': 0, 'right': 3, 'headless': 0},
-        'd': {'left': 0, 'right': 2, 'headless': 0},
-        'z': {'left': 0, 'right': 1, 'headless': 0},
-        'a': {'left': 0, 'right': 0, 'headless': 1},
-        'e': {'left': 0, 'right': 0, 'headless': 1},
-        'o': {'left': 0, 'right': 0, 'headless': 1},
-        'u': {'left': 0, 'right': 0, 'headless': 1},
+    army = {
+        'army1': {'w': 4, 'p': 3, 'b': 2, 's': 1},
+        'army2': {'m': 4, 'q': 3, 'd': 2, 'z': 1},
+        'army3': {'a': 4, 'e': 3, 'o': 2, 'u': 1, 'i': 1}
     }
-    power_left = 0
-    power_right = 0
-    power_headless = 0
+    power = {}
 
-    for char in text:
-        if side.get(char):
-            power_left += side[char]['left']
-            power_right += side[char]['right']
-            power_headless += side[char]['headless']
+    for name_army in army.keys():
+        power[name_army] = 0
+        for char in text:
+            if army[name_army].get(char):
+                power[name_army] += army[name_army].get(char)
+    max_power_befor = max(power.values())
+    max_power_army_befor = list(power.keys())[list(power.values()).index(max_power_befor)]
+    power_compare = {}
+    power_compare[max_power_army_befor] = max_power_befor
+    del power[max_power_army_befor]
+    max_power_after = max(power.values())
+    max_power_army_after = list(power.keys())[list(power.values()).index(max_power_after)]
+    power_compare[max_power_army_after] = max_power_after
 
-    if power_left > power_right and power_left > power_headless:
-        return 'Left side wins!'
-    elif power_right > power_left and power_right > power_headless:
-        return 'Right side wins!'
-    elif power_headless > power_left and power_headless > power_right:
-        return "Headless side wins!"
+    if power_compare[max_power_army_befor] != power_compare[max_power_army_after]:
+        return "" + max_power_army_befor + " wins!"
     else:
         return "Let's fight again!"
-
 
 if __name__ == '__main__':
     import doctest
