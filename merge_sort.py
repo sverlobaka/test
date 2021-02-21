bd32a04287e48c469540e61ef2e893cb424796f4
sverlobaka <73789971+sverlobaka@users.noreply.github.com>
2021-02-21 22:08:28 +0300

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,38 +1,38 @@
+def merge(a, b):
+    i = j = 0
+    result = []
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            x = a[i]
+            result.append(x)
+            i += 1
+        else:
+            x = b[j]
+            result.append(x)
+            j += 1
+
+    if j < len(b):
+        result.extend(b[j:])
+    else:
+        result.extend(a[i:])
+    return result
+
 def merge_sort(data):
     """
-    >>> merge_sort([22, 5, 1, 23, 6, 55, 89, 23, 15, 98, 9, 40, 9, 44, 3, 9, 12, 67, 87, 43, 90])
-    [1, 3, 5, 6, 9, 9, 9, 12, 15, 22, 23, 23, 40, 43, 44, 55, 67, 87, 89, 90, 98]
     >>> merge_sort([2, 1])
     [1, 2]
     >>> merge_sort([])
     []
-    >>> merge_sort([1, 0, 45, 3])
-    [0, 1, 3, 45]
-    >>> merge_sort([99, -6, 1, 23, -1, 40, 9, 5, 9999999999])
-    [-6, -1, 1, 5, 9, 23, 40, 99, 9999999999]
-    >>> merge_sort([99, -6, 1, 23, 3.5, 0, -3.5,-1, 40, 9, 5, 9999999999])
-    [-6, -3.5, -1, 0, 1, 3.5, 5, 9, 23, 40, 99, 9999999999]
     """
 
-    if len(data) > 2:
-        list_index = len(data) - 1
+    if len(data) < 2:
+        return data
 
-        for i in range(list_index):
-            min_value = data[i]
-            min_index = i
+    mid_data = len(data) // 2
+    left, right = data[:mid_data], data[mid_data:]
+    sorted_left, sorted_right = merge_sort(left), merge_sort(right)
+    return merge(sorted_left, sorted_right)
 
-            for j in range(i + 1, list_index + 1):
-                if min_value > data[j]:
-                    min_value = data[j]
-                    min_index = j
-
-            if min_index != i:
-                data[i], data[min_index] = data[min_index], data[i]
-
-    elif len(data) == 2 and data[0] > data [1]:
-        data[0], data[1] = data[1], data[0]
-
-    return data
 
 if __name__ == '__main__':
     import doctest
