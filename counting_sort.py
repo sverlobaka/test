7fcdc42f11fd8ff60a95cd8dccc12b0af47e8bee
sverlobaka <73789971+sverlobaka@users.noreply.github.com>
2021-02-22 11:37:04 +0300

    Create counting_sort.py

---
+++ counting_sort.py
@@ -0,0 +1,22 @@
+def counting_sort(numbers):
+    """
+    >>> merge([])
+    []
+    >>> merge([2])
+    [2, 1]
+    >>> merge([2, 1, 1, 1, 1])
+    [[2,1], [1,4]]
+    """
+    max_numbers = max(numbers)
+    counters = [0] * (max_numbers + 1)
+
+    for i in range(len(numbers)):
+        counters[numbers[i]] += 1
+
+    pos = 0
+    for j in range(len(counters)):
+        for i in range(counters[j]):
+            numbers[pos] = j
+            pos += 1
+
+    return counters
