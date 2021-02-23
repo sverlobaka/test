f2698a5d171c30b253704a8e9400542376d2c2cb
sverlobaka <73789971+sverlobaka@users.noreply.github.com>
2021-02-23 13:23:42 +0300

    Update counting_sort.py

--- counting_sort.py
+++ counting_sort.py
@@ -8,20 +8,34 @@ def counting_sort(numbers):
     [1, 1, 1, 1, 2]
     >>> counting_sort([1, 1, 1, 1])
     [1, 1, 1, 1]
+    >>> counting_sort([-8])
+    [-8]
+    >>> counting_sort([2, 1, -8, 1, 0, 1, 1, -1, 3, 3, -1, 7])
+    [-8, -1, -1, 0, 1, 1, 1, 1, 2, 3, 3, 7]
     """
     if len(numbers) < 1:
         return numbers
     else:
         max_elem = max(numbers)
-        counters = [0] * (max_elem + 1)
+        min_elem = min(numbers)
+        positive = [0] * (max_elem + 1)
+        negative = [0] * (abs(min_elem - 1))
 
         for n in numbers:
-            counters[n] += 1
+            if n >= 0:
+                positive[n] += 1
+            else:
+                n = abs(n)
+                negative[n] += 1
 
         result = []
-        for i in range(len(counters)):
-            if counters[i] != 0:
-                x = [i] * counters[i]
+        for i in range((len(negative) - 1), -1, -1):
+            if negative[i] != 0:
+                x = [i * (-1)] * negative[i]
+                result.extend(x)
+        for i in range(len(positive)):
+            if positive[i] != 0:
+                x = [i] * positive[i]
                 result.extend(x)
 
     return result
