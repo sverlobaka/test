5e8a131b496724182cf9f5ba596d77d797dfc592
sverlobaka <73789971+sverlobaka@users.noreply.github.com>
2021-03-02 22:55:00 +0300

    Update numbers_multiplier.py

--- numbers_multiplier.py
+++ numbers_multiplier.py
@@ -6,14 +6,32 @@ def numbers_multiplier(numbers):
     [160, 80, 80, 32, 20]
     >>> numbers_multiplier([])
     []
+    >>> numbers_multiplier([1, 2, 0, 5, 0])
+    [0, 0, 0, 0, 0]
+    >>> numbers_multiplier([1, 2, 0, 5, 8])
+    [0, 0, 80, 0, 0]
     """
 
+    zero = numbers.count(0)
     multiplier = 1
-    for char in numbers:
-        multiplier *= char
-
-    result = [int(multiplier / char) for char in numbers]
-    return result
+    if zero > 1:
+        result = [0 * char for char in numbers]
+        return result
+    elif zero == 1:
+        index_zero = int()
+        for i, char in enumerate(numbers):
+            if char != 0:
+                multiplier *= char
+            else:
+                index_zero = i
+        result = [0 * char for char in numbers]
+        result[index_zero] = multiplier
+        return result
+    else:
+        for char in numbers:
+            multiplier *= char
+        result = [int(multiplier / char) for char in numbers]
+        return result
 
 if __name__ == '__main__':
     import doctest
