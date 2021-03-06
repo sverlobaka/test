2270695f610a5bf12dffe72e6b32d773d0fd489c
sverlobaka <73789971+sverlobaka@users.noreply.github.com>
2021-03-06 22:38:51 +0300

    Create twice_and_once.py

---
+++ twice_and_once.py
@@ -0,0 +1,33 @@
+def twice_and_once(numbers):
+    """
+    >>> twice_and_once([])
+    >>> twice_and_once([1, 2])
+    >>> twice_and_once([1, 1])
+    >>> twice_and_once([1])
+    1
+    >>> twice_and_once([1, 1, 2])
+    2
+    >>> twice_and_once([1, 1, 2, 3, 3, 4, 4, 8, 8])
+    2
+    >>> twice_and_once([3, 3, 7, 7, 10, 11, 11])
+    10
+    """
+
+    if len(numbers) == 1:
+        return numbers[0]
+    elif len(numbers) < 0:
+        return
+    elif len(numbers) == 2:
+        if numbers[:1] == numbers[1:]:
+            return
+    else:
+        mid_numbers = len(numbers) // 2
+        if numbers[mid_numbers] == numbers[mid_numbers - 1] or numbers[mid_numbers] == numbers[mid_numbers + 1]:
+            left_numbers, right_numbers = numbers[:mid_numbers], numbers[mid_numbers:]
+            left_part, right_part = twice_and_once(left_numbers), twice_and_once(right_numbers)
+        else:
+            return numbers[mid_numbers]
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
