e90d476f45c12d1db0a8639abdb4e848a8eb327c
sverlobaka <73789971+sverlobaka@users.noreply.github.com>
2021-03-07 12:54:09 +0300

    Update twice_and_once.py

--- twice_and_once.py
+++ twice_and_once.py
@@ -7,26 +7,28 @@ def twice_and_once(numbers):
     1
     >>> twice_and_once([1, 1, 2])
     2
+    >>> twice_and_once([1, 2, 2])
+    1
+    >>> twice_and_once([1, 2, 3])
     >>> twice_and_once([1, 1, 2, 3, 3, 4, 4, 8, 8])
     2
-    >>> twice_and_once([3, 3, 7, 7, 10, 11, 11])
-    10
+
     """
 
     if len(numbers) == 1:
         return numbers[0]
-    elif len(numbers) < 1:
-        return
+    elif len(numbers) == 0:
+        False
     elif len(numbers) == 2:
         if numbers[:1] == numbers[1:]:
-            return
+            False
     else:
         mid_numbers = len(numbers) // 2
-        if numbers[mid_numbers] == numbers[mid_numbers - 1] or numbers[mid_numbers] == numbers[mid_numbers + 1]:
-            left_numbers, right_numbers = numbers[:mid_numbers], numbers[mid_numbers:]
-            left_part, right_part = twice_and_once(left_numbers), twice_and_once(right_numbers)
-        else:
-            return numbers[mid_numbers]
+        if numbers[mid_numbers] != numbers[mid_numbers - 1] and numbers[mid_numbers] == numbers[mid_numbers + 1]:
+            return numbers[mid_numbers - 1]
+        elif numbers[mid_numbers] == numbers[mid_numbers - 1] and numbers[mid_numbers] != numbers[mid_numbers + 1]:
+            return numbers[mid_numbers + 1]
+
 
 if __name__ == '__main__':
     import doctest
